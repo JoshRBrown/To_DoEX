5e8c0855d1f95dacb15f56d17fd758a2e8d810bd
Josh Brown <jbrown016@gmail.com>
2018-06-22 11:00:34 -0400

    sample queries created

---
+++ exquery.sql
@@ -0,0 +1,36 @@
+
+-- get all the todos
+select * from todolist;
+
+
+-- get one todo by id
+select * from todolist
+where id=2;
+
+
+-- get all unfinished tasks
+select * from todolist
+where isdone=false;
+
+-- get all finished tasks
+select * from todolist
+where isdone=true;
+
+-- search by title
+select * from todolist
+where title ilike '%sc%';
+
+-- mark item as finished
+update todolist
+    set isdone=true
+    where id=1;
+
+-- mark item as unfinished
+update todolist
+    set isdone=false
+    where id=2;
+
+-- change title text
+update todolist
+    set title='blue'
+    where id=1;
